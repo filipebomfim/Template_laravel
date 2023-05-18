2bd0a1b85d802da596f57387e40828d180a87534
Filipe Furtado <lipe.bomfim99@gmail.com>
2023-05-18 10:25:41 -0300

    Configuração do JWT para login via API

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request){
+        $credentials = $request->all(['email','password']);
+
+        //autenticação
+        $token = auth('api')->attempt($credentials);
+        
+        return $token ? 
+                response()->json(['token'=>$token],200) 
+                : 
+                response()->json(['erro'=>'Usuário ou senha inválidos!'],403);
+        
+    }
+
+    public function logout(){
+        auth('api')->logout();
+        return response()->json(['mensagem'=>'Logout realizado com sucesso!']);
+    }
+
+    public function refresh(){
+        $token = auth('api')->refresh();
+        return response()->json(['Novo token'=>$token]);
+    }
+
+    public function me(){
+        return response()->json(auth()->user());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->middleware('jwt.auth')->group(function(){
+    Route::post('me',[App\Http\Controllers\AuthController::class, 'me']);
+    Route::post('refresh',[App\Http\Controllers\AuthController::class, 'refresh']);
+    Route::post('logout',[App\Http\Controllers\AuthController::class, 'logout']);
+});
+
+Route::post('login',[App\Http\Controllers\AuthController::class, 'login']);
+
+
